2086a4fa96f8397a97d5956347b1940beb80c90f
Felipe Sá Freire <felipe.freire@fabricadecodigos.com.br>
2020-04-17 14:54:32 -0300

    Fundamentos de  Node.js

---
+++ src/models/Appointment.ts
@@ -0,0 +1,20 @@
+import { uuid } from 'uuidv4';
+
+interface AppointmentConstructor {
+  provider: string;
+  date: Date;
+}
+
+export default class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    this.id = uuid();
+    this.provider = provider;
+    this.date = date;
+  }
+}
